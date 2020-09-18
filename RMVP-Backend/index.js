2bf3d215639658cc7138c196612c19530260e006
valplusplusle <v.kummert@drmnt.de>
2020-09-18 22:55:45 +0200

    add first frontend backend connection

--- RMVP-Backend/index.js
+++ RMVP-Backend/index.js
@@ -1,5 +1,13 @@
 var express = require("express");
+var cors = require('cors');
 var app = express();
+var products = require("./products.json");
+
+app.use(cors())
+
+app.get('/', function (req, res, next) {
+    res.json(products)
+  })
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
--- rate-my-vegan-product/src/app/app.component.ts
+++ rate-my-vegan-product/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rate-my-vegan-product';
+  products: Observable<any>;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getJsonData();
+  }
+
+  getJsonData() {
+    let url = 'http://localhost:3000/';
+    fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+      this.products = out["products"];      
+    })
+    .catch(err => { throw err });
+  }
 }
