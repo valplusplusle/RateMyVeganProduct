6bb37e965f675a8e71c840f4554567b8e1288f0e
valplusplusle <v.kummert@drmnt.de>
2020-09-20 02:35:41 +0200

    add nedb, add frontend send data to backend test

--- RMVP-Backend/index.js
+++ RMVP-Backend/index.js
@@ -1,14 +1,33 @@
 var express = require("express");
 var cors = require('cors');
+const bodyParser = require('body-parser');
 var app = express();
-var products = require("./products.json");
+app.use(bodyParser.urlencoded({ extended: true }));
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: './database', autoload: true });
 
 app.use(cors())
 
 app.get('/', function (req, res, next) {
-    res.json(products)
-  })
+  db.find({}, function (err, docs) {
+    res.json(docs)
+  });
+})
+
+app.post('/newProduct', function (req, res, next) {
+  console.log('Got body:', req.body);
+  res.sendStatus(200);
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
+function addProductToDatabase() {
+    var doc = { name: 'Vegan Test Tofu'
+  , info: 'Text about the Product'
+  , picture: 'link/to/picture'
+  , score: 0
+  };
+  db.insert(doc, function (err, newDoc) {});
+}
--- rate-my-vegan-product/src/app/app.component.ts
+++ rate-my-vegan-product/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,13 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'rate-my-vegan-product';
   products: Observable<any>;
+  postId;
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getJsonData();
+    this.sendJsonData("test");
   }
 
   getJsonData() {
@@ -21,8 +24,31 @@ export class AppComponent {
     fetch(url)
     .then(res => res.json())
     .then((out) => {
-      this.products = out["products"];      
+      console.log(out)
+      this.products = out;      
     })
     .catch(err => { throw err });
   }
+
+  sendJsonData(newProduct) {
+    const data = JSON.stringify(newProduct)
+    const httpOptions = {
+      headers: ({
+          'Content-Type': 'application/x-www-form-urlencoded',
+      })
+    }
+    this.http.post("http://localhost:3000/newProduct", data, httpOptions)
+    .subscribe(
+        (val) => {
+        console.log("POST call successful value returned in body", 
+                    val);
+        },
+        response => {
+            console.log("POST call in error", response);
+        },
+        () => {
+            console.log("The POST observable is now completed.");
+        });
+  }
 }
+
